4242f2fe653d2cb77ba9f16150ba8502ad90d555
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-02-13 14:11:55 +0300

    add method for finding words, which are shoter or equal than 3 symbols

--- Program.cs
+++ Program.cs
@@ -6,3 +6,15 @@ void FillArray(string[] array)
         array[i] = Console.ReadLine();
     }
 }
+
+string[] FindWords(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            System.Console.Write(array[i] + " ");
+        }
+    }
+    return array;
+}
